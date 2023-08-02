506baa3cc7ef173b6ff55ff142ffd86ce638d35d
Bartek <b.sienkiew@gmail.com>
2023-08-02 05:36:48 +0200

    added config gathering from MQTT

--- plant_soft.ino
+++ plant_soft.ino
@@ -11,7 +11,7 @@
 
 #define LED 2
 #define CALIBRATION_BUTTON 18
-#define TIME_TO_SLEEP 60
+#define TIME_TO_SLEEP 1200  // seconds
 #define uS_TO_S_FACTOR 1000000
 
 DNSServer dnsServer;
@@ -23,34 +23,49 @@ long lastReading = 0;
 char msg[50];
 
 const bool SLEEP_MODE = true;
-const String MQTT_USER = "plantwis2e";
+const String MQTT_USER = "plantwise";
 const String AP_NAME = "PlantWise";
 const String AP_PASSWORD = "";
+const int MAX_PLANTS = 4;
+const int SENSOR_PINS[] = { 35, 34, 39, 36 };
+const int VALVE_PINS[] = { 21, 22, 23, 19 };
+
+StaticJsonDocument<1024> config;
 
 //0%: 1496 100%: 1042
 
 class Plant {
 private:
   int sensorPin;
-  int pumpPin;
+  int valvePin;
   int moisture0;
   int moisture100;
   int moisturePercentage;
   int readingAccuracy = 2;
-  int wateringTreshold;
+  int lowerTreshold;
+  int upperTreshold;
   int wateringTime;
   int wateringCycles = 0;
 
 public:
   int id;
-  Plant(int id, int sensorPin, int pumpPin, int moisture0, int moisture100, int wateringTreshold, int wateringTime) {
+  bool configured;
+  Plant() {
+    this->configured = false;
+  }
+
+  Plant(int id, int sensorPin, int valvePin, int moisture0, int moisture100, int lowerTreshold, int upperTreshold, int wateringTime) {
     this->id = id;
     this->sensorPin = sensorPin;
-    this->pumpPin = pumpPin;
+    this->valvePin = valvePin;
     this->moisture0 = moisture0;
     this->moisture100 = moisture100;
-    this->wateringTreshold = wateringTreshold;
+    this->lowerTreshold = lowerTreshold;
+    this->upperTreshold = upperTreshold;
     this->wateringTime = wateringTime;
+    this->configured = true;
+    Serial.print("Configured plant #");
+    Serial.println(this->id);
   }
 
   int getSensorData() {
@@ -80,15 +95,15 @@ public:
   }
 
   void startPump() {
-    digitalWrite(pumpPin, LOW);
+    digitalWrite(valvePin, LOW);
   }
 
   void stopPump() {
-    digitalWrite(pumpPin, HIGH);
+    digitalWrite(valvePin, HIGH);
   }
 
   bool isWateringNeeded() {
-    return readMoisturePercentage() < wateringTreshold;
+    return lowerTreshold > readMoisturePercentage();
   }
 
   void loop() {
@@ -135,12 +150,46 @@ public:
   }
 };
 
-Plant plants[] = {
-  Plant(1, 35, 21, 1496, 1042, 30, 2),
-  Plant(2, 35, 21, 1496, 1042, 30, 2),
-  Plant(3, 35, 21, 1496, 1042, 30, 2),
-  Plant(4, 35, 21, 1496, 1042, 30, 2),
-};
+Plant plants[MAX_PLANTS];
+
+void createPlantsFromConfig() {
+  JsonObject configObject = config.as<JsonObject>();
+
+  // Remove old plants
+  for (int i = 0; i < 4; i++) {
+    plants[i] = Plant();
+  }
+  int plantsConfigured = 0;
+
+  for (JsonObject::iterator it = configObject.begin(); it != configObject.end(); ++it) {
+    int plantId = atoi(it->key().c_str());
+    JsonObject plantData = it->value().as<JsonObject>();
+
+    // Check if any value is 0, skip creating the plant
+    if (plantData["moistureMin"].as<int>() == 0 ||
+        plantData["moistureMax"].as<int>() == 0 ||
+        plantData["lowerTreshold"].as<int>() == 0 ||
+        plantData["upperTreshold"].as<int>() == 0 ||
+        plantData["wateringTime"].as<int>() == 0) {
+      continue;
+    }
+
+    plants[plantId - 1] = Plant(
+      plantId,
+      SENSOR_PINS[plantId - 1],
+      VALVE_PINS[plantId - 1],
+      plantData["moistureMin"].as<int>(),
+      plantData["moistureMax"].as<int>(),
+      plantData["lowerTreshold"].as<int>(),
+      plantData["upperTreshold"].as<int>(),
+      plantData["wateringTime"].as<int>()
+    );
+    plantsConfigured++;
+  }
+
+  Serial.print(plantsConfigured);
+  Serial.println(" plants initialized from configuration.");
+}
 
 void setup() {
   Serial.begin(115200);
@@ -149,6 +198,11 @@ void setup() {
   pinMode(CALIBRATION_BUTTON, INPUT_PULLUP);
   digitalWrite(21, HIGH);
 
+  
+  for (int i = 0; i < MAX_PLANTS; i++) {
+    plants[i] = Plant();
+  }
+
   setup_wifi(SSID, PASSWORD, dnsServer, server);
   setup_ap(AP_NAME, AP_PASSWORD);
   mqtt_config(MQTT_SERVER, MQTT_PORT);
@@ -156,6 +210,7 @@ void setup() {
 
 void mqtt_reconnect() {
   client.disconnect();
+  if (WiFi.status() != WL_CONNECTED) setup_wifi(SSID, PASSWORD, dnsServer, server);
   String un = MQTT_USER + String(random(0xffff), HEX);
   if (client.connect(un.c_str())) {
     Serial.println("Connected to MQTT broker as " + un);
@@ -171,24 +226,30 @@ void mqtt_reconnect() {
 
 void mqtt_config(String MQTT_SERVER, int MQTT_PORT) {
   client.setServer(MQTT_SERVER.c_str(), MQTT_PORT);
+  client.setKeepAlive(240);
+  client.setSocketTimeout(240);
+  client.setBufferSize(500);
   mqtt_reconnect();
+
+  client.subscribe("esp/#");
   client.setCallback(callback);
 }
 
 void callback(char* topic, byte* message, unsigned int length) {
-  Serial.print("Message arrived on topic: ");
-  Serial.print(topic);
-  Serial.print(". Message: ");
-  String messageTemp;
-}
+  Serial.print("Message on topic: ");
+  Serial.println(topic);
 
+  if (String(topic) == "esp/config") {
+    deserializeJson(config, (char*)message);
+    createPlantsFromConfig();
+  }
+}
 void loop() {
   long now = millis();
   delay(10);
-  
+
   dnsServer.processNextRequest();
-  
-  if (WiFi.status() != WL_CONNECTED) setup_wifi(SSID, PASSWORD, dnsServer, server);
+
   if (!client.connected()) mqtt_reconnect();
   client.loop();
 
@@ -199,29 +260,43 @@ void loop() {
   if (now - lastReading > 5000) {
     lastReading = now;
 
+    bool anyPlantConfigured = false;
+
     for (Plant plant : plants) {
-      plant.loop();
-      String topic = "esp/plant/" + String(plant.id) + "/moisture";
-      if (client.publish(topic.c_str(), String(plant.getMoisturePercentage()).c_str()))
+      if (plant.configured) {
+        anyPlantConfigured = true;
+
+        String topic = "esp/plant/" + String(plant.id) + "/moisture";
+        if (client.publish(topic.c_str(), String(plant.readMoisturePercentage()).c_str()))
           Serial.println(String(plant.getMoisturePercentage()) + "% published");
-      else{ 
-        Serial.println("Failed to publish");
-        for (int i = 0; i < 3; i++) {
-          if (client.connected()) break;
-          mqtt_reconnect();
-          client.loop();
-          delay(5000);
-          if (client.publish(topic.c_str(), String(plant.getMoisturePercentage()).c_str())) {
-            Serial.println(String(plant.getMoisturePercentage()) + "% published");
-            break;
+        else {
+          Serial.println("Failed to publish");
+          for (int i = 0; i < 3; i++) {
+            if (client.connected()) break;
+            mqtt_reconnect();
+            client.loop();
+            delay(5000);
+            if (client.publish(topic.c_str(), String(plant.readMoisturePercentage()).c_str())) {
+              Serial.println(String(plant.getMoisturePercentage()) + "% published");
+              break;
+            }
           }
         }
       }
-
     }
+
     Serial.println("===============================================");
 
-    if (SLEEP_MODE) {
+    // SLEEPING MODES
+    if (!anyPlantConfigured) {
+      Serial.println("No plants configured");
+      client.publish("esp/error", "Failed to read config!");
+      delay(200);
+      WiFi.mode(WIFI_OFF);
+      esp_sleep_enable_timer_wakeup(120 * uS_TO_S_FACTOR); // Sleep for 2 minutes
+      Serial.println("Going to sleep for 2 minutes");
+      esp_deep_sleep_start();
+    } else if (SLEEP_MODE) {
       delay(200);
       WiFi.mode(WIFI_OFF);
       esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
